e7329bc5508bd012ec2cce7a5ddae3f493e0d9ad
Cameron White <camw85@mac.com>
2015-06-06 11:15:21 -0400

    scaffolding initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name commentationApp
+ * @description
+ * # commentationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('commentationApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
